4835bdb8846cb5c28a3ccf71f9aedb1a288385dd
hiroro-work <hiroro@sonicgarden.jp>
2022-06-09 14:37:56 +0900

    update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import { crx } from '@crxjs/vite-plugin';
+import WindiCSS from 'vite-plugin-windicss';
 import manifest from './manifest.json';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), crx({ manifest })],
+  plugins: [react(), crx({ manifest }), WindiCSS()],
 });
