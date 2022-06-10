5bce374dcfe5af87b8dc4fb2e3ecf84e67c2b3ed
hiroro-work <hiroro@sonicgarden.jp>
2022-06-10 14:32:55 +0900

    fix a bit

--- src/popup.tsx
+++ src/popup.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import Popup from '@/components/Popup';
+import Popup from '@/components/pages/Popup';
 import 'virtual:windi.css';
 import '@/styles/popup.css';
 
